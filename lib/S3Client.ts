a51b86c2e212579e17ddb01aab241c875d157b8c
anshbrahmbhatt <120459538+anshbrahmbhatt@users.noreply.github.com>
2026-04-29 13:04:54 +0530

    Update S3Client.ts

--- lib/S3Client.ts
+++ lib/S3Client.ts
@@ -1,10 +1,13 @@
 import "server-only";
-
 import { S3Client } from "@aws-sdk/client-s3";
 import { env } from "./env";
 
 export const S3 = new S3Client({
-  region: "auto",
+  region: env.S3_REGION,
   endpoint: env.AWS_ENDPOINT_URL_S3,
   forcePathStyle: false,
+  credentials: {
+    accessKeyId: env.S3_ACCESS_KEY_ID,
+    secretAccessKey: env.S3_SECRET_ACCESS_KEY,
+  },
 });
