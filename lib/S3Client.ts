ef714196bd8ecea1c1f9ffbcc2d44a28101ba620
Ansh <anshbrahmbhatt2004@gmail.com>
2026-04-30 14:22:42 +0530

    updated env variables for Netlify

--- lib/S3Client.ts
+++ lib/S3Client.ts
@@ -3,11 +3,11 @@ import { S3Client } from "@aws-sdk/client-s3";
 import { env } from "./env";
 
 export const S3 = new S3Client({
-  region: env.AWS_REGION,
+  region: env.S3_REGION,
   endpoint: env.AWS_ENDPOINT_URL_S3,
   forcePathStyle: false,
   credentials: {
-    accessKeyId: env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: env.S3_ACCESS_KEY_ID,
+    secretAccessKey: env.S3_SECRET_ACCESS_KEY,
   },
 });
